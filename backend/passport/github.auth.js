fe0eea846d203f78f3c6d15065baf298bacc67b1
aryanvisualize <aryanvisualize@gmail.com>
2026-08-10 23:15:25 +0530

    Callback url updated

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -20,7 +20,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "/api/auth/github/callback"
+            callbackURL: "https://gitverse-26yg.onrender.com/api/auth/github/callback"
         },
 
         async function (accessToken, refreshToken, profile, done) {
